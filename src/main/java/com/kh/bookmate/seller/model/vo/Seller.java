b8ab8ef9f172fe424c282c571405c525d7e2b2bb
hrkang333 <hrkang333@naver.com>
2021-10-29 21:06:51 +0900

    seller push - 10월 29일

--- src/main/java/com/kh/bookmate/seller/model/vo/Seller.java
+++ src/main/java/com/kh/bookmate/seller/model/vo/Seller.java
@@ -1,5 +1,29 @@
 package com.kh.bookmate.seller.model.vo;
 
+import java.sql.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
 public class Seller {
 
+	private String sellerNo;
+	private String sellerId;
+	private String sellerNickN;
+	private String sellerEmail;
+	private String sellerPhone;
+	private String sellerDel;
+	private int sellerDel_p1;
+	private int sellerDel_p2;
+	private String sellerAddress;
+	private Date seller_enrollDate;
+	private Date seller_modifyDate;
+	private String status;
 }
