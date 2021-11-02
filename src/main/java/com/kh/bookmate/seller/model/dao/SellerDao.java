f2f916257d18bc60f80689222889a6fd1455e470
hrkang333 <hrkang333@naver.com>
2021-11-02 21:06:35 +0900

    seller 가입 & 조회 진행중 - 11/2

--- src/main/java/com/kh/bookmate/seller/model/dao/SellerDao.java
+++ src/main/java/com/kh/bookmate/seller/model/dao/SellerDao.java
@@ -1,5 +1,26 @@
 package com.kh.bookmate.seller.model.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.bookmate.seller.model.vo.Seller;
+import com.kh.bookmate.user.model.vo.User;
+
+@Repository
 public class SellerDao {
+/*
+	public Seller loginSeller(SqlSessionTemplate sqlSession, Seller s) {
+		return sqlSession.selectOne("sellerMapper.loginSeller", s);
+	}*/
+
+	public int insertSeller(SqlSession sqlSession, Seller s) {
+		return sqlSession.insert("sellerMapper.insertSeller",s);
+	}
+
+	public Seller loginSeller(SqlSessionTemplate sqlSession, User u, Seller s) {
+
+		return sqlSession.selectOne("sellerMapper.loginSeller", s);
+	}
 
 }
--- src/main/java/com/kh/bookmate/seller/model/service/SellerServiceImpl.java
+++ src/main/java/com/kh/bookmate/seller/model/service/SellerServiceImpl.java
@@ -1,5 +1,45 @@
 package com.kh.bookmate.seller.model.service;
 
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.kh.bookmate.seller.model.dao.SellerDao;
+import com.kh.bookmate.seller.model.vo.Seller;
+import com.kh.bookmate.user.model.vo.User;
+
+@Service
 public class SellerServiceImpl implements SellerService {
 
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Autowired
+	private SellerDao sellerDao;
+
+	@Override
+	public Seller loginSeller(BCryptPasswordEncoder bCryptPasswordEncoder, User u, Seller s) {
+		Seller loginSeller = sellerDao.loginSeller(sqlSession, u, s);
+		
+		return loginSeller;
+	}
+	/*
+	@Override
+	public Seller loginSeller(User u, Seller s) {
+		Seller loginSeller = sellerDao.loginSeller(sqlSession, u, s);
+		
+		return loginSeller;
+	}*/
+	
+	@Override
+	public void insertSeller(Seller s) {
+		int result = sellerDao.insertSeller(sqlSession,s);
+		if(result < 0 ) {
+			throw new RuntimeException("판매자 가입 실패");
+		}
+	}
+
+	
 }
