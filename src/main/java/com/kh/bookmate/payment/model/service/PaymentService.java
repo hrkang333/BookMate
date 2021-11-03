7ca6fbd389551fa174985b0aae20b19972f18817
Meesou <huny0529@gmail.com>
2021-11-03 21:30:07 +0900

    미소-21-11-03

--- src/main/java/com/kh/bookmate/payment/model/service/PaymentService.java
+++ src/main/java/com/kh/bookmate/payment/model/service/PaymentService.java
@@ -1,5 +1,13 @@
 package com.kh.bookmate.payment.model.service;
 
+import java.util.ArrayList;
+
+import com.kh.bookmate.payment.model.vo.Payment;
+import com.kh.bookmate.user.model.vo.User;
+
 public interface PaymentService {
 
+	ArrayList<Payment> SelectMyOrderList(User loginUser);
+
+	
 }
